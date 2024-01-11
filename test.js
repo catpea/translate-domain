e6c4d59c1aeb7b54894ca19ce11ec715e71f76ff
catpea <corporate.miou@gmail.com>
2024-01-10 20:25:28 -0500

    New Release

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-# translate-domain
-OOP based numeric domain/range translation.
-
-```JavaScript
+#!/usr/bin/env node
+import assert from 'node:assert';
 
 import TranslateDomain from './TranslateDomain.js';
 
-// Establish Domain Ranges
 const basicDomain = new TranslateDomain([0, 100], [0, 1]);
 const nonZeroBased = new TranslateDomain([100, 200], [1, 2]);
 const macroDomain = new TranslateDomain([0, 1], [0,500]);
 
-// Perform Operations
 assert.equal( basicDomain.translate(50), .5)
 assert.equal( basicDomain.invert(50), .5)
 
@@ -19,5 +15,3 @@ assert.equal( nonZeroBased.invert(150), 1.5)
 
 assert.equal( macroDomain.translate(.50), 250)
 assert.equal( macroDomain.invert(.75), 500-(500/4))
-
-```
